07c872b2990c9d1744f836acdc2677a4faaaad70
olafire <olafire03@gmail.com>
2022-10-22 13:12:02 +0100

    Add project files.

---
+++ Two_sums - LeetCode/TwoSums.cs
@@ -0,0 +1,39 @@
+﻿
+int[] nums = new int[] { 3, 2, 3 };
+int target = 6;
+TwoSum(nums, target);
+
+int[] TwoSum(int[] nums, int target)
+{
+    var res = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums.Length >= 3)
+        {
+            if (i < target)
+            {
+                var complement = target - nums[i];
+                if (complement < target)
+                {
+                    for (int s = 0; s < nums.Length; s++)
+                    {
+                        if (complement == nums[s])
+                        {
+                            res[0] = i;
+                            res[1] = s;
+                        }
+
+                    }
+                }
+            }
+        }
+        else if (nums.Length <= 2)
+        {
+            return res = new int[] { 0, 1 };
+        }
+
+    }
+    return res;
+}
+
+
